0010319e81939b830bebb1eb7e844d8e0815d592
sajim-khan <shossain1427@gmail.com>
2023-03-01 02:40:00 +0600

    added index file

--- type.js
+++
@@ -1,6 +0,0 @@
-const a = 5; // int
-const s = 'My name is sajim' // string
-student = {firstName:'sajim', lastname:'Khan'} // object
-array = [21,34,43,54,56]  // array
-friends = ['Hossain', 'Sajim'] 
-
